706e7b634e513ba9d015a1b36a376f168700b221
hector paz <hectordpazs@gmail.com>
2022-01-29 17:37:43 -0500

    app

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
+app.use(express.static('public'));
+
 app.get('/usuario', function (req, res) {
   res.json('GET usuario')
 })

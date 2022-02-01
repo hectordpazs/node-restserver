8ac212fbe15d1ec0ad3bc7c851d3348a3743c795
hector paz <hectordpazs@gmail.com>
2022-02-01 15:51:50 -0500

    app

--- server/database/config.js
+++ server/database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async ()=>{
     try{
 
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.urlDB);
 
         console.log('DB Online');
 
